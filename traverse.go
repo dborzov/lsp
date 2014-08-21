bdb302e5e29722dfe6627629745aff1acde8f283
Peter Borzov <peter@sowingo.com>
2014-08-21 16:23:42 -0400

    scaffold trie traverse iterator

---
+++ traverse.go
@@ -0,0 +1,10 @@
+// traversing group Trie and applying grouping and filters and stuff
+package main
+
+func Traverse() func() []*FileInfo {
+	// var Stack []string
+	// var Keys []string
+	return func() []*FileInfo {
+		return []*FileInfo{}
+	}
+}
