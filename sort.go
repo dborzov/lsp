fe91579878af616e87600171c8477cb93378c414
Peter Borzov <pborzov@kineticcafe.com>
2016-03-16 11:21:13 -0400

    Fix alphabetized ordering

--- sort.go
+++ sort.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"strings"
 	"unicode"
 	"unicode/utf8"
 )
@@ -11,9 +12,22 @@ type alphabeticSort []*FileInfo
 func (a alphabeticSort) Len() int      { return len(a) }
 func (a alphabeticSort) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a alphabeticSort) Less(i, j int) bool {
-	rune1, _ := utf8.DecodeRuneInString(a[i].f.Name())
-	rune2, _ := utf8.DecodeRuneInString(a[j].f.Name())
-	return unicode.ToLower(rune1) < unicode.ToLower(rune2)
+	r1 := strings.NewReader(a[j].f.Name())
+	r2 := strings.NewReader(a[i].f.Name())
+	for {
+		ch1 := nextRune(r1)
+		ch2 := nextRune(r2)
+		if ch1 == ch2 {
+			if ch1 == utf8.RuneError {
+				return true
+			}
+			continue
+		}
+		if ch1 > ch2 {
+			return true
+		}
+		return false
+	}
 }
 
 type sizeSort []*FileInfo
@@ -30,4 +44,12 @@ func (a timeSort) Len() int      { return len(a) }
 func (a timeSort) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a timeSort) Less(i, j int) bool {
 	return a[i].f.ModTime().After(a[j].f.ModTime())
+}
+
+func nextRune(r *strings.Reader) rune {
+	ch, _, err := r.ReadRune()
+	if err != nil {
+		return utf8.RuneError
+	}
+	return unicode.ToLower(ch)
 }
